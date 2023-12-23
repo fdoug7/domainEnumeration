766ce6f4aa9204117d3616c27ac5a138c1f13dd9
fdoug7 <94262944+fdoug7@users.noreply.github.com>
2023-12-24 08:13:11 +1100

    Dictionary Update
    Now stores values in nested dictionaries. Clean up of the code is still needing to be done.

--- nmap-python.py
+++ nmap-python.py
@@ -9,7 +9,8 @@ import dns.resolver as dr
 import nmap as nm  
 
 
-domains = []
+
+domains = {"domains": {} }
 serverNames = []
 
 nmScan = nm.PortScanner()
@@ -28,12 +29,12 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--file', help='File')
     args = parser.parse_args()
 
-    print(args)
 
     
 
 
 class domainEnumer:
+    ##Template domain = [["domain": {}, "mxname": {"ip": }]]
 
 
     def getMX():
@@ -44,36 +45,38 @@ class domainEnumer:
                 
             with open(domain_list_file, 'r') as f:
                 for line in f.readlines():
-                    domains.append(line.strip())
-
+                    domains['domains'].update({line.strip(): {}})
+                    domains['domains'][line.strip()].update({"mxnames": {}})
+                    
         elif args.domain:
-            domains.append(args.domain)
+            domains['domains'].update({args.domain: {}})
+            domains['domains'][args.domain].update({"mxnames": {}})
             
         else:
             print('Missing potential arguments.')
             sys.exit(1)
-        
-        if domains:
-            for domain in domains:
+                
+
+        if True:
+            for domain in domains['domains']:
                 print('\b')
                 print('='*30)
                 print(f'{domain}')
                 print('-'*30)
                 try:
+                    print('Server MX names found: \b')
                     for x in dr.resolve(domain, 'MX'):
                         t = x.to_text()
                         i = t.find(' ')
-                        n = t[i+1:-1]
-                        serverNames.append(n)
+                        n = t[i+1:-1]   
+                        print(x)     
+                        domains['domains'][domain]['mxnames'].update({n: {}})            
+
                 except dr.NoAnswer:
                     print('Error querying the domain')
                 except dr.NXDOMAIN:
-                    print('DNS query name does not exist')  
-            print('Server MX names found: \b')
-            for i in serverNames:
-                print(f'{i}')  
-        else:
-            print('No domains provided')
+                    print('DNS query name does not exist')
+
 
     def getTXT():
         if args.file:
@@ -94,34 +97,46 @@ class domainEnumer:
         for domain in domains:
             print(dr.resolve(domain, 'TXT').rrset)
 
+
     def getIP():
-        serverIP = []
-        for name in serverNames:
-            for y in dr.resolve(name, 'A'):
-                z = y.to_text()
-                serverIP.append(z)
-        serverIP = list(set(serverIP)) 
-        return serverIP
+        for i in domains['domains']:
+            for x in domains['domains'][i]['mxnames']:
+                domains['domains'][i]['mxnames'][x].update({"ip": {}})
+
+                for y in dr.resolve(x, 'A'):
+                    z = y.to_text()
+                    domains['domains'][i]['mxnames'][x]['ip'].update({z: {}})         
+        
+
 
 
     def serverScan():
-        for ip in domainEnumer.getIP():
-            print('='*30)
-            nmScan.scan(ip, str(port))
-            
-            print("IP: " + ip)
-            print("IP Status: ", nmScan[ip].state())
-            print("Port state: ", nmScan[ip]['tcp'][port]['state'] if nmScan[ip].get('tcp')  else "")
-            print('\b')
+
+        for i in domains['domains']:
+            for x in domains['domains'][i]['mxnames']:
+                for ip in domains['domains'][i]['mxnames'][x]['ip']:
+                    try:
+                        print('='*30)
+                        nmScan.scan(ip, str(port))
+                            
+                        print("IP: " + ip)
+                        print("IP Status: ", nmScan[ip].state())
+                        domains['domains'][i]['mxnames'][x]['ip'][ip].update({"IP Status": nmScan[ip].state()})
+                        print("Port state: ", nmScan[ip]['tcp'][port]['state'] if nmScan[ip].get('tcp')  else "")
+                        domains['domains'][i]['mxnames'][x]['ip'][ip].update({"Port State": nmScan[ip]['tcp'][port]['state']})
+
+                        print('\b')
+                    except KeyError as err: 
+                        print(f"Unexpected {err=}, {type(err)=}")
+        print(domains)
+
 
 
 if args.spfrecords:
     domainEnumer.getTXT()
 elif args.mxrecords:
     domainEnumer.getMX()
+    domainEnumer.getIP()
+    domainEnumer.serverScan()
 else:
     print('Missing arguments')
-
-
-# getMXDN()
-# serverScan()
